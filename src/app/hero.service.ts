3968294335b8d159444579d13de26b12e3091da7
Alexander S. Makarevich <alexander.makarevich@eltex.loc>
2018-01-25 19:19:34 +0700

    Tap into the Observable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -25,10 +25,9 @@ export class HeroService {
 
   /** GET heroes from the server */
   getHeroes (): Observable<Hero[]> {
-    // Todo: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(
+        tap(heroes => this.log(`fetched heroes`)),
         catchError(this.handleError('getHeroes', []))
       );
   }
