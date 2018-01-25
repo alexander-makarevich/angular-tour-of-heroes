5da9c3a6493e3283e52f0c609a3d32c210e58160
Alexander S. Makarevich <alexander.makarevich@eltex.loc>
2018-01-25 10:30:31 +0700

    Create MessagesComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroService } from './hero.service'
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
